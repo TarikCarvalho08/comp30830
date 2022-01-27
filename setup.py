3c54b7393680b6172c21936a0a02ebcd47cda5e4
TarikCarvalho08 <tarik.carvalho@ucdconnect.ie>
2022-01-27 09:49:38 +0000

    adding new packages

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='systemInformation.py',
+      version='0.1',
+      description='System Information',
+      url='fileFolder\__init__.py',
+      author='Tarik Carvalho',
+      author_email='tarik.caravalho@ucdconnect.ie',
+      license='MIT',
+      packages=['fileFolder'],
+      zip_safe=False)
+
