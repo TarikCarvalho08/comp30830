3d29bab428a9375d426201f4beeb0ca0f27470bf
TarikCarvalho08 <tarik.carvalho@ucdconnect.ie>
2022-01-30 18:04:14 +0000

    implementing a python package

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='systemInformation',
+setup(name='systemInfo',
       version='0.1',
       description='System Information',
-      url='fileFolder\__init__.py',
+      url='',
       author='Tarik Carvalho',
       author_email='tarik.caravalho@ucdconnect.ie',
       license='MIT',
       packages=['fileFolder'],
+      entry_points={'console_scripts':['systemInfo=fileFolder.systemInfo:main']},
       zip_safe=False)
 
